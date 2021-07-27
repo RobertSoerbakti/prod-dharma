f40791baf716d69c4e152c54afad54a48b0e88e3
Robert Soerbakti <rsuranta@dyned.com>
2021-07-27 12:58:18 +0700

    #First Deploy

---
+++ precache-manifest.b56c6f6aa1b9f904cafd9cfe73e2dc74.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3d619e6d2be6f8c244",
+    "url": "/css/app.38b11092.css"
+  },
+  {
+    "revision": "0573b9231a8316427ad6e751b52e87a4",
+    "url": "/fonts/Poppins-Black.0573b923.ttf"
+  },
+  {
+    "revision": "3fb21c8084013f3d0176bc98bcf76e60",
+    "url": "/fonts/Poppins-BlackItalic.3fb21c80.ttf"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/fonts/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "09775bde3e9448b38c063b746e21cb6b",
+    "url": "/fonts/Poppins-BoldItalic.09775bde.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/fonts/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "29f7dd016eeed2bcd79ba482eb3f27ec",
+    "url": "/fonts/Poppins-ExtraBoldItalic.29f7dd01.ttf"
+  },
+  {
+    "revision": "86a2f13e91ac85080ebaeaab9463b9f1",
+    "url": "/fonts/Poppins-ExtraLight.86a2f13e.ttf"
+  },
+  {
+    "revision": "05139b6509a2baa8f188fbade78fc3ed",
+    "url": "/fonts/Poppins-ExtraLightItalic.05139b65.ttf"
+  },
+  {
+    "revision": "5e956c44060a7b3c0e39819ae390ab15",
+    "url": "/fonts/Poppins-Italic.5e956c44.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/fonts/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "1eaf3af47612e6163a2e27e847c6ac7d",
+    "url": "/fonts/Poppins-LightItalic.1eaf3af4.ttf"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/fonts/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "1749e4b603749026393f64506a3bcbbe",
+    "url": "/fonts/Poppins-MediumItalic.1749e4b6.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/fonts/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/fonts/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "378a091bc1b1e6e6d6327beb6bfb07b9",
+    "url": "/fonts/Poppins-SemiBoldItalic.378a091b.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/fonts/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "c93e22e98b7a8d58f83ce42b278815eb",
+    "url": "/fonts/Poppins-ThinItalic.c93e22e9.ttf"
+  },
+  {
+    "revision": "e919b642ecf0440e86d58bf3974ecdb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bf966829528e72e0716",
+    "url": "/js/about.b4577186.js"
+  },
+  {
+    "revision": "3c3d619e6d2be6f8c244",
+    "url": "/js/app.df1277cf.js"
+  },
+  {
+    "revision": "810d8efee83470dee895",
+    "url": "/js/chunk-vendors.a87a069a.js"
+  },
+  {
+    "revision": "f1c4a734ec56b0bef12739f5eca50a9b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
